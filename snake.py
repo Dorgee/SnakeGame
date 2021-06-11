08e3a9e115fb3b498a087be4ebde10263dec4857
Dorgee <dorgee.lama@edu.sait.ca>
2021-06-10 23:30:31 -0600

    Added food and fixed some bugs

--- snake.py
+++ snake.py
@@ -1,7 +1,6 @@
-import snake
-from turtle import *
-
+from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -11,36 +10,37 @@ RIGHT = 0
 class Snake:
 
     def __init__(self):
-        self.turtle_body = []
+        self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for p in STARTING_POSITIONS:
-            turtle = Turtle("square")
-            turtle.color("white")
-            turtle.penup()
-            turtle.setposition(p)
-            self.turtle_body.append(turtle)
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
     def move(self):
-        for t in range(len(self.turtle_body) - 1, 0, -1):
-            new_x = self.turtle_body[t - 1].xcor()
-            new_y = self.turtle_body[t - 1].ycor()
-            self.turtle_body[t].goto(new_x, new_y)
-        self.turtle_body[0].fd(20)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.turtle_body[0].heading() != DOWN:
-            self.turtle_body[0].setheading(UP)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        if self.turtle_body[0].heading() != UP:
-            self.turtle_body[0].setheading(DOWN)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        if self.turtle_body[0].heading() != RIGHT:
-            self.turtle_body[0].setheading(LEFT)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        if self.turtle_body[0].heading() != LEFT:
-            self.turtle_body[0].setheading(RIGHT)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
